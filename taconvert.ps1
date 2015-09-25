e1a6522a0ecff024870a27d158a05e1dd4fa98c3
funkybaboon <funkybaboon@users.noreply.github.com>
2015-09-25 23:57:33 +0200

    add 2015 version

--- taconvert.ps1
+++ taconvert.ps1
@@ -1,28 +1,49 @@
 # Get Start Time
 $startDTM = (Get-Date)
 
-$array = @()
-$array += { convert.exe -density 200 -quality 80 -delete 0 -scene 1  C:\Users\Benni\Desktop\ta2014\v33_1_21_Northland.pdf C:\Users\Benni\Desktop\ta2014\%03d.jpg }
-$array += { convert.exe -density 200 -quality 80 -delete 0 -scene 22 C:\Users\Benni\Desktop\ta2014\v33_22_31_Auckland.pdf C:\Users\Benni\Desktop\ta2014\%03d.jpg }
-$array += { convert.exe -density 200 -quality 80 -delete 0 -scene 32 C:\Users\Benni\Desktop\ta2014\v33_32_49_Waikato.pdf C:\Users\Benni\Desktop\ta2014\%03d.jpg }
-$array += { convert.exe -density 200 -quality 80 -delete 0 -scene 50 C:\Users\Benni\Desktop\ta2014\v33_50_62_Whanganui.pdf C:\Users\Benni\Desktop\ta2014\%03d.jpg }
-$array += { convert.exe -density 200 -quality 80 -delete 0 -scene 63 C:\Users\Benni\Desktop\ta2014\v33_63_69_Manawatu.pdf C:\Users\Benni\Desktop\ta2014\%03d.jpg }
-$array += { convert.exe -density 200 -quality 80 -delete 0 -scene 70 C:\Users\Benni\Desktop\ta2014\v33_70_75_Wellington.pdf C:\Users\Benni\Desktop\ta2014\%03d.jpg }
-$array += { convert.exe -density 200 -quality 80 -delete 0 -scene 76 C:\Users\Benni\Desktop\ta2014\v33_76_92_Marlborough.pdf C:\Users\Benni\Desktop\ta2014\%03d.jpg }
-$array += { convert.exe -density 200 -quality 80 -delete 0 -scene 93 C:\Users\Benni\Desktop\ta2014\v33_93_117_Canterbury.pdf C:\Users\Benni\Desktop\ta2014\%03d.jpg }
-$array += { convert.exe -density 200 -quality 80 -delete 0 -scene 118 C:\Users\Benni\Desktop\ta2014\v33_118_127_Otago.pdf C:\Users\Benni\Desktop\ta2014\%03d.jpg }
-$array += { convert.exe -density 200 -quality 80 -delete 0 -scene 128 C:\Users\Benni\Desktop\ta2014\v33_128_141_Southland.pdf C:\Users\Benni\Desktop\ta2014\%03d.jpg }
+# Maximum numbers of simultaneous conversions
+$MaxThreads = 4
 
+# Get current path, set version and target
+$version = "v34"
+$path = (Get-Item -Path ".\" -Verbose).FullName + "\"
+$target = $path + "%03d.jpg"
+
+# 2 dimensional array. first element map, second element scene
+$array = @()
+$array += ,("_1_21_Northland.pdf", "1")
+$array += ,("_22_31_Auckland.pdf", "22")
+$array += ,("_32_49_Waikato.pdf", "32")
+$array += ,("_50_62_Whanganui.pdf", "50")
+$array += ,("_63_69_Manawatu.pdf", "63")
+$array += ,("_70_75_Wellington.pdf", "70")
+$array += ,("_76_92_Marlborough.pdf", "76")
+$array += ,("_93_117_Canterbury.pdf", "93")
+$array += ,("_118_127_Otago.pdf", "118")
+$array += ,("_128_141_Southland.pdf", "128")
+
+# convert pdf to jpg with 200dpi, 80% quality
+$ConvertJob = { 
+	param($scene, $map, $target) 
+	convert.exe -density 200 -quality 80 -delete 0 -scene $scene $map $target
+}
+
+# Start processes
 foreach ($element in $array) {
-   $MaxThreads = 4
+   $scene = $element[1]
+   $map = $path + $version + $element[0]
+
    While (@(Get-Job | Where { $_.State -eq "Running" }).Count -ge $MaxThreads)
    {  Write-Host "Waiting for open thread...($MaxThreads Maximum)"
       Start-Sleep -Seconds 3
    }
+
+   Write-Host "Converting $map"
  
-   Start-Job $element
+   Start-Job $ConvertJob -Arg $scene, $map, $target
 }
 
+# Check for running processes
 While (@(Get-Job | Where { $_.State -eq "Running" }).Count -ne 0)
 {  Write-Host "Waiting for background jobs..."
    Get-Job    #Just showing all the jobs
@@ -37,4 +58,6 @@ ForEach ($Job in (Get-Job)) {
 $endDTM = (Get-Date)
 
 # Echo Time elapsed
-"Elapsed Time: $(($endDTM-$startDTM).totalseconds) seconds"
+$ts = $endDTM-$startDTM
+$tsString = '{0:00}m {1:00}s' -f $ts.Minutes,$ts.Seconds
+Write-Host "Elapsed Time: $tsString"
